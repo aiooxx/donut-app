522f70293719928f0289220033793496dbf81a32
aiooxx <aiooxxx@gmail.com>
2014-07-12 02:00:37 +0900

    first commit

---
+++ Assets/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+
+	void Start () {
+		
+	}
+	
+
+	void Update () {
+	
+	}
+
+	void OnMouseDown(){
+		//GameObject obj = GameObject.Find("enemyPrefab(Clone)");
+		Destroy(this.gameObject);
+		Debug.Log(this.gameObject);
+	}
+
+	void OnDestroy(){
+		GameObject go = GameObject.Find("GameManager");
+			if (go != null) {
+				GameManager gm = go.GetComponent(typeof(GameManager)) as GameManager;
+				gm.AddScore(100);
+			}
+	}
+}
---
+++ Assets/GameManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject enemyPrefab;
+	public GUIText time;
+	public GUIText score;
+
+	//int myScore = 0;
+	int pointObject = 10;
+	int isupdate = 1;
+	float remainingTime = 0f;
+
+	// Use this for initialization
+	void Start () {
+		MakeEnemys();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float deltaTime = Time.deltaTime;
+		if(isupdate == 1){
+			remainingTime += deltaTime;
+			time.text = ("" + remainingTime.ToString("f3"));
+			//score.text = ("score:" + myScore);
+		}
+	}
+
+	//enemy make
+	void MakeEnemys(){
+		for (int i = 0; i< 10; i++) {
+			GameObject enemy = (GameObject)Instantiate(enemyPrefab);
+			Vector3 p = enemy.transform.position;
+			p.x = Random.Range(-4,4);
+			p.y = Random.Range(-4,4);
+			enemy.transform.position = p;
+		}
+	}
+
+	public void AddScore(int add){
+		if (remainingTime > 0) {
+			//myScore += add;
+			pointObject -= 1;
+			}
+		if(pointObject == 0){
+			isupdate = 0;
+		}
+	}
+
+
+}
